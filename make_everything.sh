f3a64647881ed3237ca4ca0cbc90891aab3bccfd
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2015-12-15 19:16:48 -0200

    spliting GDT asm from C code

--- make_everything.sh
+++ make_everything.sh
@@ -6,7 +6,8 @@ DIR=$PWD
 cd $DIR/kernel
 
 nasm -f aout -o start.o start.S
-nasm -f aout -o interrupts.o interrupts.S
+nasm -f aout -o interrupts.o int.S
+nasm -f aout -o globaldt.o gdt.S
 /usr/bin/gcc -m32 -Wall -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
 /usr/bin/gcc -m32 -Wall -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o int.o int.c
 /usr/bin/gcc -m32 -Wall -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o kb.o kb.c
@@ -21,6 +22,7 @@ nasm -f aout -o interrupts.o interrupts.S
 ld -mi386linux -T link.ld -o kernel.bin 	\
 	start.o					\
 	interrupts.o				\
+	globaldt.o				\
 	main.o					\
 	int.o					\
 	kb.o					\
